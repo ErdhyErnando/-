0e62e23ae151fc7935824fc9a1363807c4301fde
Erdhy Ernando <ernandodavis1002@gmail.com>
2024-01-16 16:54:18 +0100

    7. Add Food Page and Fix Pages Folder Directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import AppRoutes from './AppRoutes';
-import Header from './components/Header/Header';
+import AppRoutes from "./AppRoutes";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <>
-  <Header />
-  <AppRoutes />
-  </>
-  
+  return (
+    <>
+      <Header />
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
+import FoodPage from "./pages/Food/FoodPage";
 
 export default function AppRoutes() {
   return (
@@ -7,6 +8,7 @@ export default function AppRoutes() {
       <Route path="/" element={<HomePage />} />
       <Route path="/search/:searchTerm" element={<HomePage />} />
       <Route path="/tag/:tag" element={<HomePage />} />
+      <Route path="/food/:id" element={<FoodPage />} />
     </Routes>
   );
 }
