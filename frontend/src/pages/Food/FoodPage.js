5faf4c81697b8ab1a162a933805ca5c5fb1c4f73
andrewbn23 <andrewbn23@gmail.com>
2024-01-16 22:28:54 +0100

    8&9 adding-cart & notfound

--- frontend/src/pages/Food/FoodPage.js
+++ frontend/src/pages/Food/FoodPage.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
 import classes from "./foodPage.module.css";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
 import { getById } from "../../services/foodService";
 import StarRating from "../../components/StarRating/StarRating";
 import Tags from "../../components/Tags/Tags";
 import Price from "../../components/Price/Price";
+import NotFound from "../../components/NotFound/NotFound";
 
 export default function FoodPage() {
   const [food, setFood] = useState({});
   const { id } = useParams();
+  const { addToCart } = useCart();
+  const navigate = useNavigate();
+
+  const handleAddToCart = () => {
+    addToCart(food);
+    navigate("/cart");
+  };
 
   useEffect(() => {
     getById(id).then(setFood);
@@ -16,7 +25,9 @@ export default function FoodPage() {
 
   return (
     <>
-      {food && (
+      {!food ? (
+        <NotFound message="Food Not Found!" linkText="Back To Home Page" />
+      ) : (
         <div className={classes.container}>
           <img
             className={classes.image}
@@ -63,7 +74,7 @@ export default function FoodPage() {
               <Price price={food.price} />
             </div>
 
-            <button>Add to Cart</button>
+            <button onClick={handleAddToCart}>Add to Cart</button>
           </div>
         </div>
       )}
